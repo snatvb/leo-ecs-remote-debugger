092bbbacc554a0dd34bc3c66c81233ecc0e0b2ff
Andrew Brghton <snatlip48@gmail.com>
2020-03-26 22:58:34 +0300

    Removed mock data

--- src/renderer/transport/connectionHandler.ts
+++ src/renderer/transport/connectionHandler.ts
@@ -1,16 +1,6 @@
+import { IServerService } from '@renderer/types/serverService'
 import { store } from '@store'
-import { ConnectionEventType, IServerService } from '.'
-
-// TODO:  TO REMOVE
-const w = store.createWorld(432)
-w.createEntity(2, 0)
-w.isAlive = false
-const w2 = store.createWorld(4442)
-w2.createEntity(232, 0)
-w2.createEntity(423, 0)
-w2.createEntity(23432, 0)
-w2.createEntity(23642, 0)
-w2.isAlive = false
+import { ConnectionEventType } from '.'
 
 const handleConnected = (id: number) => {
   store.createWorld(id)
