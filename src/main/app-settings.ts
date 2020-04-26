78b0988e9ac419a88faecc4e435c59ceb2920701
Andrew Brghton <snatlip48@gmail.com>
2020-04-26 20:47:50 +0300

    Fix settings load error and add config for build

--- src/main/app-settings.ts
+++ src/main/app-settings.ts
@@ -1,11 +1,12 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
+import { Either } from 'monad-maniac'
 import * as path from 'path'
 import { readJsonFile } from './fs/readJsonFile'
 import { writeFile } from './fs/writeFile'
 
 const FILE_NAME = 'app-settings.json'
 
-const getFilePath = () => path.join(app.getPath('userData'), FILE_NAME)
+const getFilePath = () => path.join(process.env.PORTABLE_EXECUTABLE_DIR || app.getPath('userData'), FILE_NAME)
 
 export enum EventName {
   Save = 'settings-save',
@@ -25,16 +26,17 @@ const defaultSettings: Settings = {
   }
 }
 
-const validate = (settings: { [key: string]: any }): boolean => {
+const validate = (settings: { [key: string]: any }): Either.Shape<Error, Settings> => {
   if (!settings.ws) {
-    return false
+    return Either.left(new Error(`Web socket settings was not found`))
   }
 
-  if (typeof settings !== 'number') {
-    return false
+  // tslint:disable-next-line: no-unsafe-any
+  if (typeof settings.ws.port !== 'number') {
+    return Either.left(new Error('Settings web socket port is not a number'))
   }
 
-  return true
+  return Either.right(settings as Settings)
 }
 
 let cache: Settings
@@ -51,7 +53,14 @@ export const load = async () => {
   }
 
   const json = await readJsonFile(filePath)
-  cache = json.filter(validate).getOrElse(defaultSettings) as Settings
+  cache = json.chain(validate).caseOf({
+    Right: (x) => x,
+    Left: (error) => {
+      console.warn('[WARNING] Error in read settings', error)
+
+      return defaultSettings
+    },
+  })
 
   return cache
 }
